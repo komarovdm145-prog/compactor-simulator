4481e3821befceb407534c4ba8832d6efa5c27a6
komarovdm145-prog <komarovdm145@gmail.com>
2026-09-19 07:46:45 +0300

    v8.6.16: restore stable main routes

--- home.js
+++ home.js
@@ -11,8 +11,8 @@ $$('[data-difficulty]').forEach(b=>b.onclick=()=>{opt.difficulty=b.dataset.diffi
 $('#optionsBtn').onclick=()=>$('#optionsModal').classList.remove('hidden');
 $('#aboutBtn').onclick=()=>$('#aboutModal').classList.remove('hidden');
 $$('[data-close]').forEach(b=>b.onclick=()=>b.closest('.modal').classList.add('hidden'));
-$('#startBtn').onclick=()=>location.href='./game-v8615.html?v=8.6.15#start';
+$('#startBtn').onclick=()=>location.href='./game.html?v=8.6.16#start';
 $('#trainingBtn').onclick=()=>location.href='./training.html?v=8.6.11';
 $('#trainingTestBtn').onclick=()=>location.href='./test.html?v=6';
-$('#examBtn').onclick=()=>location.href='./game-v8615.html?v=8.6.15#exam';
+$('#examBtn').onclick=()=>location.href='./game.html?v=8.6.16#exam';
 sync();
